77942214e2a52d27c2f8573168c6f43329796eae
YogeshBerwalIITM <21f1006363@ds.study.iitm.ac.in>
2025-01-30 09:34:28 +0530

    Created a sqrt() function, print the square root untill convergace with some tolerance value

--- square_root.py
+++ square_root.py
@@ -1,8 +1,17 @@
-x=100.0
-s=1
 
-kargs=100
-for i in range(kargs):
-    s=0.5*(s+x/s)
 
-print(f"The square root of {x} is {s}")
+def sqrt():
+    x=10000.0
+    s=1.0
+    
+    kargs=100
+    tol=0.1e-14
+    for i in range(kargs):
+        s0=s
+        s=0.5*(s+x/s)
+        delta_s=abs(s-s0)
+        if(delta_s<tol):
+            break
+        print(f"At {i+1} iteration, the value of quareroot is {s}")
+
+    print(f"The square root of {x} is {s}")
