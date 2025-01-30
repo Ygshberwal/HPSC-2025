324aedb876218e582dbc0f3bd0e4bae6e9af91a9
YogeshBerwalIITM <21f1006363@ds.study.iitm.ac.in>
2025-01-30 09:48:42 +0530

    Added another function for sqrt2(x) which takes input as an arguement

--- square_root.py
+++ square_root.py
@@ -12,6 +12,23 @@ def sqrt():
         delta_s=abs(s-s0)
         if(delta_s<tol):
             break
-        print(f"At {i+1} iteration, the value of quareroot is {s}")
+        print(f"At {i+1} iteration, the value of squareroot is {s}")
+
+    print(f"The square root of {x} is {s}")
+
+
+def sqrt2(x):
+    
+    s=1.0
+    
+    kargs=100
+    tol=0.1e-14
+    for i in range(kargs):
+        s0=s
+        s=0.5*(s+x/s)
+        delta_s=abs(s-s0)
+        if(delta_s<tol):
+            break
+        print(f"At {i+1} iteration, the value of squareroot is {s}")
 
     print(f"The square root of {x} is {s}")
