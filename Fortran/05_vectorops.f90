f743fce09cf0ba2643b32e8d3f5d0fd0777cdf23
YgshBerwal <yogeshberwal266@gmail.com>
2025-02-22 10:44:26 +0530

    Vector/Array Opertations

---
+++ Fortran/05_vectorops.f90
@@ -0,0 +1,24 @@
+program vectorops
+	implicit none
+	real(kind=8), dimension(3) :: x,y
+
+	x=(/10, 20, 30/)
+	y=(/100., 25., 64./)
+
+	print*, "x= :"
+	print*, x
+	
+	print*, "y= :"
+        print*, y
+
+	print*, "x**2 + y = "
+	print*, x**2+y
+
+	print*, "sqrt(y) ="
+	print*, sqrt(y)
+
+	print*, "dot_product(x,y)b ="
+	print*, dot_product(x,y)
+
+end program vectorops
+	
